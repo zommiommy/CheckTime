c13d7b8f49ec5d2ed5b169e4c9778387289d9ce3
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2019-08-13 11:39:27 +0200

    Update neteye4.py

--- src/neteye4.py
+++ src/neteye4.py
@@ -9,7 +9,7 @@ class WinScript(MainClass):
     def __init__(self):
         super(WinScript, self).__init__()
         os_depenat_required = self.parser.add_argument_group('os dependant settings (required) Windows')
-        os_depenat_required.add_argument("-H", "--hostnames",    help="host which disks will be checked.", type=str, required=True)
+        os_depenat_required.add_argument("-H", "--host",    help="host which disks will be checked.", type=str, required=True)
         os_depenat_required.add_argument("-s", "--service", help="service to be checked.",            type=str, required=True)
         os_depenat_optional = self.parser.add_argument_group('os dependant settings (optional) Windows')
         os_depenat_optional.add_argument("-m", "--metric", help="metric to be checked.", type=str, action="append", default=[])
