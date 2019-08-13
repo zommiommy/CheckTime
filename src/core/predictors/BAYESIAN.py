dabd6ba8bed872f05b1406464ebba6e01d06936c
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2019-08-13 11:58:06 +0200

    BugFix error in importing the BayesianRidge regression

--- src/core/predictors/BAYESIAN.py
+++ src/core/predictors/BAYESIAN.py
@@ -6,7 +6,7 @@ from typing import Tuple
 from sklearn.linear_model import BayesianRidge
 
 def BAYESIAN(x : np.ndarray, y : np.ndarray) -> Tuple[int, int]:
-    clf = linear_model.BayesianRidge()
+    clf = BayesianRidge()
     clf.fit(x, y)
     m, q = reg.coef_[0], reg.intercept_z
     return m, q
