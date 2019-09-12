029f09ac6a404e2606894d2cbf666b766bc53526
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2019-09-12 16:29:36 +0200

    Added time predicted cap

--- src/core/predict_time.py
+++ src/core/predict_time.py
@@ -7,6 +7,8 @@ from core.utils import Timer
 from core.logger import logger
 from core.predictors import *
 
+MAX_TIME = 3 * (52 * 7 * 24 * 60 * 60 * 1e9) # in nanoseconds
+
 def predict_time_left(x : np.ndarray, y : np.ndarray, name : str, mode : str = "BAYESIAN") -> int:
     functions = {
         "OLS":OLS,
@@ -22,11 +24,14 @@ def predict_time_left(x : np.ndarray, y : np.ndarray, name : str, mode : str = "
         with Timer("The prediction took {time}s"):
             m, q, p =  functions[mode](x, y)
             logger.info("The coefficents predicted are m [{m}] q[{q}]".format(**locals()))
-        if m <= 0:
+        if m <= MAX_TIME:
             logger.info("The predicted line is not growing so it will never reach the max")
             return "inf", p
         time_predicted = (1 - q)/m
         delta = time_predicted - x[-1]
+        if delta > MAX_TIME:
+            logger.info(f"The predicted time [{delta}] is over [{MAX_TIME}] so it's casted to [inf]")
+            return "inf", p
         return delta[0], p
     logger.error("Mode [%s] not found, the available ones are %s"%(mode, functions.keys()))
     return None, 0
