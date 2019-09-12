eaf17f7e06cdf32ef1ac4875a326391f7da4637b
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2019-09-12 15:19:46 +0200

    Changed probability prediction method

--- src/core/predictors/BAYESIAN.py
+++ src/core/predictors/BAYESIAN.py
@@ -4,12 +4,14 @@
 import numpy as np
 from typing import Tuple
 from sklearn.linear_model import BayesianRidge
+from sklearn.metrics import r2_score
 from scipy.stats import chisquare, norm
 
 def BAYESIAN(x : np.ndarray, y : np.ndarray) -> Tuple[int, int]:
     clf = BayesianRidge()
     clf.fit(x, y)
     m, q = clf.coef_[0], clf.intercept_
-    mean, std = clf.predict(y.reshape(-1, 1), return_std=True)
-    p = np.nanmean([norm(loc=mean, scale=std).pdf(v) for v, m, s in zip(y, mean, std)])
+    mean = clf.predict(y.reshape(-1, 1))
+    # This it's not an actual probability but it should be interpretable as one.
+    p = np.clip(r2_score(mean, y), 0, 1)
     return m, q, p
